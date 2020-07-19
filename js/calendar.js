76c917607ca37415ab49a7724c365c7b84f740ff
marvel0usx <marvel0usx@outlook.com>
2020-07-18 23:25:40 -0600

    [dev] improved appearance

--- js/calendar.js
+++
@@ -1,7 +0,0 @@
-window.onload = () => {
-    document.querySelector("#ignite").addEventListener("click", (button) => {
-        document.querySelector("#inf").style.display = "block";
-        document.querySelector("#inf").style.zIndex = "-1";
-        button.style.display = "none";
-    });
-};
