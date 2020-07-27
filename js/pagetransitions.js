7d57c3906781a9c1de94bd5b625c5c3f39a3217f
marvel0usx <marvel0usx@outlook.com>
2020-07-26 21:29:45 -0600

    [debug] fixed section title transition

--- js/pagetransitions.js
+++ js/pagetransitions.js
@@ -1,8 +1,10 @@
 var currPage, nextPage;
 var currPageAnimEnd, nextPageAnimEnd;
 var isAnimating;
+const HOME_ROUTE = "/home.html";
 
 window.addEventListener("DOMContentLoaded", () => {
+    showGreetingAndLoadHome();
     window.addEventListener("message", receiveMsg);
 });
 
@@ -52,4 +54,23 @@ function resetPages() {
     nextPage.className = "pt-page pt-current-page";
     currPage.innerHTML = "";
     isAnimating = false;
+}
+
+function showGreetingAndLoadHome() {
+    let body = document.querySelector("body");
+    const greeting = `<div class="greetings">
+                        <h2>Ahoy! Stranger.</h2>
+                        <h2>Welcome to this cyberIsland of Haoyan</h2>
+                        <h2>It's a pleasure to meet you.</h2>
+                        <button id="go">Let's Explore</button>
+                      </div>`;
+    body.innerHTML = greeting + body.innerHTML;
+    let btn;
+    do {
+        btn = document.querySelector("#go");
+    } while (!btn);
+    btn.addEventListener("click", () => {
+        body.removeChild(document.querySelector("div.greetings"));
+        document.querySelector("iframe.pt-current-page").src = HOME_ROUTE;
+    });
 }
